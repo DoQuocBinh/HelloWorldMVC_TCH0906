7e7a4d32c59f9cae8410b71be7ed1f377107a939
DoQuocBinh <binhdq@fpt.edu.vn>
2021-08-05 08:52:10 +0700

    Add project files.

---
+++ HelloWorldMVC/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldMVC.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [Range(10,20,ErrorMessage ="between 10 and 20")]
+        public decimal Price { get; set; }
+        public bool IsOnSale { get; set; }
+
+        [Required(ErrorMessage ="Must enter picture")]
+        public string PictureUrl { get; set; }
+
+    }
+}
